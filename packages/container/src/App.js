9f765d486ad2a1d18591f0175da4a761d75cf218
Sujono Chen <sujonochen@Sujonos-MacBook-Pro.local>
2021-01-16 16:11:35 +0800

    Update App

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import MarketingApp from './components/MarketingApp';
 
-console.log('final test');
-
 export default () => {
   return (
     <div>
-      <h1>Hi There</h1>
+      <h1>Hi There!</h1>
       <hr />
       <MarketingApp />
     </div>
